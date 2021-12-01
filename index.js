b011e989c20162392ccd7ef21f6e87bea1ef13b3
+ <+>
2021-11-30 20:54:31 -0700

    Completed Assignment JS - 2 - Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    const allCats = [...cats, name];
+    return allCats;
+}
+
+function prependCat(name){
+    const allCats2 = [name, ...cats];
+    return allCats2;
+}
+
+function removeLastCat() {
+    const mostCats = cats.slice(0,cats.length - 1);
+    return mostCats;
+}
+
+function removeFirstCat() {
+    const manyCats = cats.slice(1);
+    return manyCats;
+}
